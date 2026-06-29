d2937a647e90048a89b233262d470f90fcd669cd
Swapnil Patil <swapnilp1609@gmail.com>
2026-06-29 11:41:01 +0530

    Time: 0 ms (100%), Space: 23 MB (83.26%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -14,14 +14,16 @@ public:
     int height(TreeNode* root){
         if(root==NULL) return 0;
         int lh = height(root->left);
+        if(lh==-1) return -1;
         int rh = height(root->right);
-        return 1+max(lh,rh);
+        if(rh==-1) return -1;
+        if(abs(lh-rh)>1) return -1;
+        return 1 + max(lh,rh);
     }
     bool isBalanced(TreeNode* root) {
-        if(root==NULL) return true;
-        int lh = height(root->left);
-        int rh = height(root->right);
-        if(abs(lh-rh)>1) return false;
-        return (isBalanced(root->left) && isBalanced(root->right));
+        if(height(root)==-1){
+            return false;
+        }
+        return true;
     }
 };
