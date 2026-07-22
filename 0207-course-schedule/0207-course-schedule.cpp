4f250f4ac89269fee30790f6f757d08c4b842702
Swapnil Patil <swapnilp1609@gmail.com>
2026-07-22 12:16:26 +0530

    Time: 4 ms (63.55%), Space: 19.6 MB (42.31%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool canFinish(int v, vector<vector<int>>& prerequisites) {
+
+        vector<int> adj[v];
+
+        for(auto it : prerequisites){
+            adj[it[1]].push_back(it[0]);
+        }
+
+        vector<int> indegree(v,0);
+
+        for(int i=0;i<v;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+
+        queue<int> q;
+
+        for(int i=0;i<v;i++){
+            if(indegree[i]==0)
+                q.push(i);
+        }
+
+        vector<int> topo;
+
+        while(!q.empty()){
+
+            int node=q.front();
+            q.pop();
+
+            topo.push_back(node);
+
+            for(auto it:adj[node]){
+
+                indegree[it]--;
+
+                if(indegree[it]==0)
+                    q.push(it);
+            }
+        }
+
+        return topo.size()==v;
+    }
+};
