93e5e09bb716095bcbb3dcda4981c50115d9a91e
Swapnil Patil <swapnilp1609@gmail.com>
2026-08-05 16:29:17 +0530

    Time: 19 ms (51.1%), Space: 47.1 MB (32.58%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,74 @@
+class DisjointSet {
+	vector<int> rank,parent,size;
+	public:
+		DisjointSet(int n){
+			rank.resize(n+1,0);  // if 1 based indexing
+			parent.resize(n+1);
+			size.resize(n+1);
+			for(int i=0;i<n;i++){
+				parent[i] = i;
+				size[i] = 1;
+			}
+		}
+		// path compression
+		int findUPar(int node){
+			if(node == parent[node]){
+				return node;
+			}
+			parent[node] = findUPar(parent[node]);
+			return parent[node];
+		}
+		void unionByRank(int u,int v){
+			int ulp_u = findUPar(u);
+			int ulp_v = findUPar(v);
+			if(ulp_u == ulp_v) return ;
+			if(rank[ulp_u] < rank[ulp_v]){
+				parent[ulp_u] = ulp_v;
+			}
+			else if(rank[ulp_v] < rank[ulp_u]){
+				parent[ulp_v] = ulp_u;
+			}
+			// if both have same rank attach anyone with each other
+			else {
+				parent[ulp_v] = ulp_u;
+				rank[ulp_u]++;
+			}
+		}
+		void unionBySize(int u,int v){
+			int ulp_u = findUPar(u);
+			int ulp_v = findUPar(v);
+			if(ulp_u == ulp_v) return ;
+			if(size[ulp_u] < size[ulp_v]){
+				parent[ulp_u] = ulp_v;
+				size[ulp_v] += size[ulp_u];
+			}
+			else{
+				parent[ulp_v] = ulp_u;
+				size[ulp_u] += size[ulp_v];
+			}
+		}
+};
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int cntExtra = 0;
+        for(auto it : connections){
+            int u = it[0];
+            int v = it[1];
+            if(ds.findUPar(u) == ds.findUPar(v)){
+                cntExtra++;
+            }
+            else{
+                ds.unionBySize(u,v);
+            }
+        }
+        int cntC = 0;
+        for(int i=0;i<n;i++){
+            if(ds.findUPar(i)==i) cntC++;
+        }
+        int ans = cntC-1;
+        if(cntExtra>=ans) return ans;
+        return -1;
+    }
+};
