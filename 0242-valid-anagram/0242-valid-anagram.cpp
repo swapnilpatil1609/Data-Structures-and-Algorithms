1487af717be16b335a8029e184531ca5f57a50a1
Swapnil Patil <swapnilp1609@gmail.com>
2026-08-08 10:19:10 +0530

    Time: 1 ms (54.52%), Space: 9.9 MB (11.59%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp;
+        if(s.size()!=t.size()){
+            return false;
+        }
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]--;
+            if(mp[t[i]]<0) return false;
+        }
+        return true;
+    }
+};
