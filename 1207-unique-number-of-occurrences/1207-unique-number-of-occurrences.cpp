0a3aa0585b7b762592fc4184679372d531c9ef81
Swapnil Patil <swapnilp1609@gmail.com>
2026-08-08 20:18:23 +0530

    Time: 0 ms (100%), Space: 12.2 MB (17.33%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int n = arr.size();
+        unordered_map<int,int> mp;
+        unordered_set<int> st;
+        for(int i=0;i<n;i++){
+            mp[arr[i]]++;
+        }
+        for(auto it:mp){
+            if(st.find(it.second) != st.end()){
+                return false;
+            }
+            st.insert(it.second);
+        }
+        return true;
+    }
+};
