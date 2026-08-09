13f2ca6aeca6e4b1d02e009bb5ac2e2a26ae3043
Swapnil Patil <swapnilp1609@gmail.com>
2026-08-09 10:32:19 +0530

    Time: 0 ms (100%), Space: 27.3 MB (16.11%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        reverse(s.begin(),s.end());
+        int n = s.size();
+        int l = 0;
+        int r = n-1;
+        while(l<r){
+            swap(s[l],s[r]);
+            l++;
+            r--;
+        }
     }
 };
