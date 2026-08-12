f54f657f4cc7285cc047e1c3032db0a583864686
Swapnil Patil <swapnilp1609@gmail.com>
2026-08-12 15:02:07 +0530

    Time: 8 ms (5.44%), Space: 113.8 MB (61.93%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -6,10 +6,10 @@ public:
         for(int i=0;i<k;i++){
             sum+=nums[i];
         }
-        double maxsum=sum;
+        double maxsum = sum;
         for(int i=k;i<n;i++){
-            sum+=nums[i]-nums[i-k];
-            maxsum=max(maxsum,sum);
+            sum=sum+nums[i]-nums[i-k];
+            maxsum = max(maxsum,sum);
         }
         return (double)maxsum/k;
     }
