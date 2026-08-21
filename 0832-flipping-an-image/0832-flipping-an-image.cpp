55d1b0676b1f789c0cd86718821c6f379bc984e2
Swapnil Patil <swapnilp1609@gmail.com>
2026-08-21 07:59:53 +0530

    Time: 3 ms (6.66%), Space: 12.7 MB (36.34%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.cpp
+++ 0832-flipping-an-image/0832-flipping-an-image.cpp
@@ -7,7 +7,7 @@ public:
             reverse(image[i].begin(),image[i].end());
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
+            for(int j=0;j<m;j++){
                 if(image[i][j]==1) image[i][j]=0;
                 else image[i][j]=1;
             }
