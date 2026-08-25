6423028416d4143bb0f3a2ac983cfcb5e34d78f4
Swapnil Patil <swapnilp1609@gmail.com>
2026-08-25 18:13:13 +0530

    Time: 8 ms (81.77%), Space: 11.7 MB (81.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -9,14 +9,12 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*,int> mp;
-        ListNode* temp = head;
-        while(temp!=NULL){
-            if(mp.find(temp)!=mp.end()){
-                return true;
-            }
-            mp[temp]++;
-            temp=temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) return true;
         }
         return false;
     }
