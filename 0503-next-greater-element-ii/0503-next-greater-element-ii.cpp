f0a3f4072af76bc88c3383d61861f4577f3c5e64
Swapnil Patil <swapnilp1609@gmail.com>
2026-09-18 11:54:32 +0530

    Time: 0 ms (100%), Space: 28.5 MB (38.18%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         stack<int> st;
         vector<int> nge(n);
-        for(int i =2*n-1;i>=0;i--){
+        for(int i=2*n-1;i>=0;i--){
             while(!st.empty() && st.top()<=nums[i%n]){
                 st.pop();
             }
